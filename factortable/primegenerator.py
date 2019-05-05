4a2585528151ed1ee962470bf4d20b43bb85d099
Rafael Monico <rmonico1@gmail.com>
2019-05-05 15:43:28 -0300

    Implemented algoritmn to calculate prime number on demmand

--- factortable/primegenerator.py
+++ factortable/primegenerator.py
@@ -1,11 +1,31 @@
 class PrimeGenerator(object):
 
+    _first_prime = 2
+    _known_primes = [_first_prime, _first_prime + 1]
+
     def __init__(self):
         self.i = 0
-        self.primes = [2, 3, 5, 7, 11, 13, 17, 19, 23]
 
     def next(self):
-        next = self.primes[self.i]
+        if self.i == len(PrimeGenerator._known_primes):
+            PrimeGenerator._known_primes.append(self._calculate_next_prime())
+
+        next = PrimeGenerator._known_primes[self.i]
         self.i += 1
 
         return next
+
+    def _calculate_next_prime(self):
+        n = PrimeGenerator._known_primes[-1] + PrimeGenerator._first_prime
+
+        while not self._is_prime(n):
+            n += PrimeGenerator._first_prime
+
+        return n
+
+    def _is_prime(self, n):
+        for prime in PrimeGenerator._known_primes:
+            if n % prime == 0:
+                return False
+
+        return True
